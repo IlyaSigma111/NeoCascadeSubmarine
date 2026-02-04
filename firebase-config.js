94914d0ad51bd655478505b5356584f619205141
IlyaSigma111 <ilyagulkov25@gmail.com>
2026-02-04 15:34:39 +0500

    Update firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDkW-A2TbucU89yqAmCFITqbZZ4mxg1a0k",
+    authDomain: "neocascadesubmarine.firebaseapp.com",
+    databaseURL: "https://neocascadesubmarine-default-rtdb.firebaseio.com",
+    projectId: "neocascadesubmarine",
+    storageBucket: "neocascadesubmarine.firebasestorage.app",
+    messagingSenderId: "72707896034",
+    appId: "1:72707896034:web:5eb79d1f5d7c2feff6e4bb"
+};
